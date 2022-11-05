96d069bb788d980f1d78d0a64ee43487b3b28bab
KanzaTahreem <106119105+KanzaTahreem@users.noreply.github.com>
2022-11-05 18:00:49 +0500

    Set routes for app navigation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './Components/Navbar';
+import Books from './Pages/Books';
+import Categories from './Pages/Categories';
 
 function App() {
   return (
-    <div className="App" />
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Books />} />
+        <Route path="categories" element={<Categories />} />
+      </Routes>
+    </>
   );
 }
 
