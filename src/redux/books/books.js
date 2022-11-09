606ba9152a9574faa64030646210b856665d07e7
KanzaTahreem <106119105+KanzaTahreem@users.noreply.github.com>
2022-11-09 12:09:43 +0500

    Fix error in book reducer

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -15,17 +15,11 @@ export const removeBook = (id) => ({
 
 const bookReducer = (state = initialState, action) => {
   switch (action.type) {
-    case 'ADD_BOOK':
-      return [
-        ...state,
-        action.payload,
-      ];
+    case ADD_BOOK:
+      return [...state, action.payload];
 
-    case 'REMOVE_BOOK':
-      return [
-        ...state,
-        state.filter((book) => book.id !== action.payload),
-      ];
+    case REMOVE_BOOK:
+      return [...state.filter((book) => book.id !== action.payload)];
 
     default:
       return state;
