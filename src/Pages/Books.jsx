d576e5378e1041cdeeead5224753bd1e56387083
KanzaTahreem <106119105+KanzaTahreem@users.noreply.github.com>
2022-11-09 14:52:37 +0500

    Dispatch function to remove a book

--- src/Pages/Books.jsx
+++ src/Pages/Books.jsx
@@ -2,10 +2,15 @@ import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import Book from '../Components/Book';
 import Form from '../Components/Form';
+import { removeBook } from '../redux/books/books';
 
 const Books = () => {
   const bookList = useSelector((state) => state.books);
   const dispatch = useDispatch();
+
+  const removeBookFromList = (id) => {
+    dispatch(removeBook(id));
+  };
 
   return (
     <section>
@@ -13,7 +18,7 @@ const Books = () => {
         {bookList.map((book) => (
           <li key={book.id}>
             <Book title={book.title} author={book.author} id={book.id} />
-            <button type="button">Remove</button>
+            <button type="button" onClick={() => removeBookFromList(book.id)}>Remove</button>
           </li>
         ))}
       </ul>
