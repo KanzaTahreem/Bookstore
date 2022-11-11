d865f6edb6714f63edd53bfb6fda81b6df4d0d53
KanzaTahreem <106119105+KanzaTahreem@users.noreply.github.com>
2022-11-11 21:46:46 +0500

    Add styles for the header

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,18 +1,37 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import '../styles/Navbar.css';
+import { ImUser } from 'react-icons/im';
 
 const Navbar = () => (
-  <nav>
-    <h1>BookStore</h1>
-    <ul>
-      <li>
-        <NavLink to="/" end> Books </NavLink>
-      </li>
-      <li>
-        <NavLink to="categories"> Categories </NavLink>
-      </li>
-    </ul>
-  </nav>
+  <header className="header">
+    <nav className="navbar">
+      <h1 className="title">Bookstore CMS</h1>
+      <ul className="nav-links">
+        <li className="linkItem">
+          <NavLink
+            to="/"
+            end
+            className={({ isActive }) => (isActive ? 'activelink' : undefined)}
+          >
+            Books
+          </NavLink>
+        </li>
+        <li className="linkItem">
+          <NavLink
+            to="categories"
+            className={({ isActive }) => (isActive ? 'activelink' : undefined)}
+          >
+            Categories
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+    <div className="user">
+      <ImUser />
+    </div>
+  </header>
+
 );
 
 export default Navbar;
