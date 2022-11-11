770e3457c093f85942dbd2031375b4295e0f1f42
KanzaTahreem <106119105+KanzaTahreem@users.noreply.github.com>
2022-11-11 16:03:32 +0500

    Update the identifier for the app

--- src/redux/endpoint.js
+++ src/redux/endpoint.js
@@ -1,5 +1,5 @@
-const APP_ID = 'TBbztvavCO8ZgSZUQkKt';
+const APP_ID = 'Qi2GCbiTgCYqFoRaL0sk';
 const BASE_URL = `https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/${APP_ID}`;
-const POST_BOOK = `${BASE_URL}/books`;
+const FETCH_BOOK_URL = `${BASE_URL}/books`;
 
-export default POST_BOOK;
+export default FETCH_BOOK_URL;
